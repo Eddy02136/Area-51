f79072ca47fe8337b99fa70d0765394498c95ec5
antonincmp <antonin.campi@epitech.eu>
2024-09-17 12:01:41 +0200

    add: routes, animated background

---
+++ front-end/src/pages/login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Login.css"
+
+const Login = () => {
+  return (
+    <div className='login-page'>
+        <h1>AREA 51</h1>
+        <div className='login-buttons-container'>
+            <button className='login-button-google'>Login with Google</button>
+            <button className='login-button-email'>Login with Email</button>
+        </div>
+        <div className='register-link-container'>
+            <p>Or</p>
+            <button>Create an account</button>
+        </div>
+    </div>
+  )
+}
+
+export default Login
