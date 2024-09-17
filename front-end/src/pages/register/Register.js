519c8cce18a860bb80c482d8faf3f22714ef3490
antonincmp <antonin.campi@epitech.eu>
2024-09-17 15:55:06 +0200

    add : register page

--- front-end/src/pages/register/Register.js
+++ front-end/src/pages/register/Register.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import "./Register.css"
+
 
 const Register = () => {
+
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/login')
+    }
   return (
-    <div>Register</div>
+    <div className='register-page'>
+        <h1>Create an account</h1>
+        <div className='register-inputs-container'>
+            <input type="text" className='register-input' placeholder='First Name'/>
+            <input type="text" className='register-input' placeholder='Last Name'/>
+            <input type="text" className='register-input' placeholder='Email'/>
+            <input type="text" className='register-input' placeholder='Password'/>
+            <input type="text" className='register-input' placeholder='Confirm Password'/>
+        </div>
+        <div className="register-button-email">
+            <button>Register with Email</button>
+        </div>
+        <div className='register-link-container'>
+            <p>Or</p>
+            <button className='login-button' onClick={handleClick}>Login</button>
+        </div>
+    </div>
   )
 }
 
