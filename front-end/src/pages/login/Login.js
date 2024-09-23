47acb2638b1c7b7223187e435187f6b73ef056c4
antonincmp <antonin.campi@epitech.eu>
2024-09-23 11:32:18 +0200

    add: login with email dialog

--- front-end/src/pages/login/Login.js
+++ front-end/src/pages/login/Login.js
@@ -1,26 +1,44 @@
 import "./Login.css"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import LoginEmailDialog from "../../components/login/LoginEmailDialog"
 
 const Login = () => {
-
     const navigate = useNavigate()
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+
     const handleClick = () => {
         navigate('/register')
     }
 
-  return (
-    <div className='login-page'>
-        <h1>AREA 51</h1>
-        <div className='login-buttons-container'>
-            <button className='login-button-google'>Login with Google</button>
-            <button className='login-button-email'>Login with Email</button>
+    const openDialog = () => {
+        setIsDialogOpen(true)
+    }
+
+    const closeDialog = () => {
+        setIsDialogOpen(false)
+    }
+
+    return (
+        <div className='login-page'>
+            <h1>AREA 51</h1>
+            <div className="login-page-container">
+                {!isDialogOpen && (
+                    <>
+                        <div className='login-buttons-container'>
+                            <button className='login-button-google'>Login with Google</button>
+                            <button className='login-button-email' onClick={openDialog}>Login with Email</button>
+                        </div>
+                        <div className='register-link-container'>
+                            <p>Or</p>
+                            <button className='register-button' onClick={handleClick}>Create an account</button>
+                        </div>
+                    </>
+                )}
+                {isDialogOpen && <LoginEmailDialog onCancel={closeDialog} />}
+            </div>
         </div>
-        <div className='register-link-container'>
-            <p>Or</p>
-            <button className='register-button' onClick={handleClick}>Create an account</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Login
