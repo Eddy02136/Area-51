5c99150e146619e502298ee3b6a434dd662058c8
eddy gardes <eddy.gardes@epitech.eu>
2024-09-30 11:01:43 +0200

    update: swagger doc api

--- back-end/src/users/dto/CreateUser.dto.ts
+++ back-end/src/users/dto/CreateUser.dto.ts
@@ -2,35 +2,22 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @ApiProperty({
-    description: "User's first name",
-    example: 'John',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   firstname: string;
 
-  @ApiProperty({
-    description: "User's last name",
-    example: 'Doe',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   lastname: string;
 
-  @ApiProperty({
-    description: "User's email address",
-    example: 'john.doe@example.com',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @ApiProperty({
-    description: "User's password",
-    example: 'strongPassword123',
-    minLength: 6,
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
