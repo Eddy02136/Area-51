b1db498ac93340414535a681f91a1a6b690c099f
Nakimee <eddygardes@gmail.com>
2024-10-02 19:09:13 +0200

    update: auth-url and invite requests

--- back-end/src/API/discord/discord.controller.ts
+++ back-end/src/API/discord/discord.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
-import { FastifyReply } from 'fastify';
-import { DiscordService } from './discord.service';
+import {Controller, Get, Query, Res} from '@nestjs/common';
+import {FastifyReply} from 'fastify';
+import {DiscordService} from './discord.service';
 import * as process from "node:process";
 import {ApiTags} from "@nestjs/swagger";
 
@@ -9,19 +9,16 @@ import {ApiTags} from "@nestjs/swagger";
 export class DiscordController {
   constructor(private readonly discordService: DiscordService) {}
 
-  @Get('login')
-  async discordLogin(@Res() reply: FastifyReply): Promise<void> {
+  @Get('auth-url')
+  discordLogin(): String {
     const redirectUri = encodeURIComponent(process.env.DISCORD_REDIRECT_URI);
-    const discordLoginUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=identify+email+guilds`;
-
-    reply.status(302).redirect(discordLoginUrl);
+    return `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=identify+email+guilds`;
   }
 
-  @Get('redirect')
+  @Get('callback')
   async discordLoginRedirect(@Query('code') code: string, @Res() reply: FastifyReply): Promise<void> {
     try {
       const accessToken = await this.discordService.getAccessToken(code);
-      const user = await this.discordService.getUser(accessToken);
 
       reply.send({ message: 'Discord login successful', accessToken});
     } catch (error) {
@@ -31,9 +28,8 @@ export class DiscordController {
   }
 
   @Get('invite')
-  async discordInvite(@Res() reply: FastifyReply): Promise<void> {
+  discordInvite(): String {
     const clientId = process.env.DISCORD_CLIENT_ID;
-    const discordInviteUrl: string = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot`;
-    reply.status(302).redirect(discordInviteUrl);
+    return `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot`;
   }
 }
