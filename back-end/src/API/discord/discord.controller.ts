603bbbbf32894be918552801e1d0db3552f232c6
Nakimee <eddygardes@gmail.com>
2024-10-02 17:06:18 +0200

    add: invite bot request

--- back-end/src/API/discord/discord.controller.ts
+++ back-end/src/API/discord/discord.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Query, Res } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 import { DiscordService } from './discord.service';
+import * as process from "node:process";
+import {ApiTags} from "@nestjs/swagger";
 
 @Controller('discord')
+@ApiTags('Discord')
 export class DiscordController {
   constructor(private readonly discordService: DiscordService) {}
 
   @Get('login')
   async discordLogin(@Res() reply: FastifyReply): Promise<void> {
     const redirectUri = encodeURIComponent(process.env.DISCORD_REDIRECT_URI);
-    const discordLoginUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=identify`;
-    console.log(discordLoginUrl);
+    const discordLoginUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=identify+email+guilds`;
+
     reply.status(302).redirect(discordLoginUrl);
   }
 
@@ -20,14 +23,17 @@ export class DiscordController {
       const accessToken = await this.discordService.getAccessToken(code);
       const user = await this.discordService.getUser(accessToken);
 
-      console.log(accessToken);
-      reply.send({
-        message: 'Discord login successful',
-        accessToken,
-      });
+      reply.send({ message: 'Discord login successful', user });
     } catch (error) {
-      console.error(error);
+      console.error('Error during Discord login redirect:', error);
       reply.status(500).send({ message: 'Error logging in with Discord' });
     }
+  }
+
+  @Get('invite')
+  async discordInvite(@Res() reply: FastifyReply): Promise<void> {
+    const clientId = process.env.DISCORD_CLIENT_ID;
+    const discordInviteUrl: string = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot`;
+    reply.status(302).redirect(discordInviteUrl);
   }
 }
--- back-end/src/API/discord/discord.service.ts
+++ back-end/src/API/discord/discord.service.ts
@@ -6,28 +6,33 @@ export class DiscordService {
   private readonly clientID = process.env.DISCORD_CLIENT_ID;
   private readonly clientSecret = process.env.DISCORD_CLIENT_SECRET;
   private readonly redirectUri = process.env.DISCORD_REDIRECT_URI;
+  private readonly scopes: string[] = ['identify', 'email', 'dm_channels.messages.write', 'dm_channels.messages.read', 'guilds'];
 
   async getAccessToken(code: string): Promise<string> {
-    const response = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
-      client_id: this.clientID,
-      client_secret: this.clientSecret,
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: this.redirectUri,
-    }).toString(), {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+    try {
+      const response = await axios.post('https://discord.com/api/oauth2/token', new URLSearchParams({
+        client_id: this.clientID,
+        client_secret: this.clientSecret,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: this.redirectUri,
+        scope: this.scopes.join('+'),
+      }).toString(), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
 
-    return response.data.access_token;
+      return response.data.access_token;
+    } catch (error) {
+      console.error('Error getting access token:', error.response?.status, error.response?.data || error.message);
+      throw new Error('Failed to get access token');
+    }
   }
 
   async getUser(accessToken: string): Promise<any> {
     const response = await axios.get('https://discord.com/api/users/@me', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
 
     return response.data;
