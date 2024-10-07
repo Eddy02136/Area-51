77246d5ad01618e38a48ca86c086121836403d75
Nakimee <eddygardes@gmail.com>
2024-10-07 19:46:08 +0200

    add: health route to check if api is running

---
+++ back-end/src/health/health.controller.ts
@@ -0,0 +1,17 @@
+import {Controller, Get} from "@nestjs/common";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import { HealthService } from "./health.service";
+
+
+@Controller('health')
+@ApiTags('Health')
+export class HealthController {
+    constructor(private readonly healthService: HealthService) {}
+
+    @Get('ping')
+    @ApiOperation({ summary: 'Get response from Area51 API' })
+    @ApiResponse({ status: 200, description: 'Area51 API responded successfully.' })
+    async getIssPosition() {
+        return this.healthService.getPing();
+    }
+}
