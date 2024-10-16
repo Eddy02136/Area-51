87de0a9e2d197aceb210f623967e6bddb80f5094
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-10-16 16:16:00 +0200

    add: getStateConnection list to load logged or unlogged lists from Connection list

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Connections/ConnectionChecker.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Connections/ConnectionChecker.java
@@ -3,6 +3,7 @@ package com.usukae.area.Classes.Connections;
 import android.content.Context;
 
 import com.usukae.area.Classes.Managers.SharedPreferencesManager;
+import com.usukae.area.Classes.Utils.ToolboxUtil;
 
 public class ConnectionChecker {
 
@@ -14,13 +15,19 @@ public class ConnectionChecker {
         this.preferencesManager = new SharedPreferencesManager(context);
     }
 
+    private String boolToString(boolean value) {
+        return value ? "true" : "false";
+    }
+
     public void checkConnections(Context context) {
+        ToolboxUtil toolboxUtil = new ToolboxUtil();
+
         connectionsApiProtocol.discordConnected(context, (success, code, data) -> {
-            preferencesManager.setBoolean("discord_connected", success);
+            preferencesManager.setStringList("discord_data", boolToString(success), toolboxUtil.getFormattedDate());
         });
 
         connectionsApiProtocol.spotifyConnected(context, (success, code, data) -> {
-            preferencesManager.setBoolean("spotify_connected", success);
+            preferencesManager.setStringList("spotify_data", boolToString(success), toolboxUtil.getFormattedDate());
         });
     }
 }
--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Connections/ConnectionsUtil.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Connections/ConnectionsUtil.java
@@ -14,10 +14,24 @@ public class ConnectionsUtil {
         List<Connection> connectionsList = new ArrayList<>();
         SharedPreferencesManager sharedPreferencesManager = new SharedPreferencesManager(context);
 
-        connectionsList.add(new Connection("Spotify", R.drawable.ic_spotify, sharedPreferencesManager.getBoolean("spotify_connected")));
-        connectionsList.add(new Connection("Discord", R.drawable.ic_discord, sharedPreferencesManager.getBoolean("discord_connected")));
+        connectionsList.add(new Connection("Spotify", R.drawable.ic_spotify, sharedPreferencesManager.getStringList("spotify_data")));
+        connectionsList.add(new Connection("Discord", R.drawable.ic_discord, sharedPreferencesManager.getStringList("discord_data")));
 
         return connectionsList;
+    }
+
+    public List<Connection> getStateConnections(Context context, String state) {
+        List<Connection> allConnections = getConnections(context);
+        List<Connection> validConnections = new ArrayList<>();
+
+        for (Connection connection : allConnections) {
+            List<String> data = connection.getData();
+            if (data != null && !data.isEmpty() && state.equals(data.get(0))) {
+                validConnections.add(connection);
+            }
+        }
+
+        return validConnections;
     }
 }
 
