242b2bc4123e665e4481b7de5c41dc3e4ba225de
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-10-16 16:16:30 +0200

    add: DashboardAreaAdapter.java and DashboardConnectionAdapter.java to load lists on the main page

---
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Areas/DashboardAreaAdapter.java
@@ -0,0 +1,69 @@
+package com.usukae.area.Classes.Areas;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.usukae.area.R;
+
+import java.util.List;
+
+public class DashboardAreaAdapter extends RecyclerView.Adapter<DashboardAreaAdapter.AreaViewHolder> {
+
+    private final Context context;
+    private final List<Area> areas;
+
+    private AreasUtil areasUtil;
+
+    public DashboardAreaAdapter(Context context, List<Area> areas) {
+        this.context = context;
+        this.areas = areas;
+    }
+
+    @NonNull
+    @Override
+    public AreaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.area_item_dashboard, parent, false);
+        return new AreaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AreaViewHolder holder, int position) {
+        Area area = areas.get(position);
+        createClasses();
+        holder.bind(area, areasUtil);
+    }
+
+    private void createClasses() {
+        areasUtil = new AreasUtil();
+    }
+
+    @Override
+    public int getItemCount() {
+        return areas.size();
+    }
+
+    public static class AreaViewHolder extends RecyclerView.ViewHolder {
+        private final TextView titleTextView;
+        private final ImageView pictureBehindImageView, pictureAboveImageView;
+
+        public AreaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.areaTitle);
+            pictureBehindImageView = itemView.findViewById(R.id.areaBehindPicture);
+            pictureAboveImageView = itemView.findViewById(R.id.areaAbovePicture);
+        }
+
+        public void bind(Area area, AreasUtil areasUtil) {
+            titleTextView.setText(area.getName());
+            pictureBehindImageView.setImageResource(areasUtil.getAreaIcon(area.getReaction()));
+            pictureAboveImageView.setImageResource(areasUtil.getAreaIcon(area.getAction()));
+        }
+    }
+}
---
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Connections/DashboardConnectionAdapter.java
@@ -0,0 +1,66 @@
+package com.usukae.area.Classes.Connections;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.usukae.area.R;
+
+import java.util.List;
+
+public class DashboardConnectionAdapter extends RecyclerView.Adapter<DashboardConnectionAdapter.ConnectionViewHolder> {
+
+    private final List<Connection> connections;
+    private final Context context;
+
+    public DashboardConnectionAdapter(Context context, List<Connection> connections) {
+        this.context = context;
+        this.connections = connections;
+    }
+
+    @NonNull
+    @Override
+    public ConnectionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.connection_item_dashboard, parent, false);
+        return new ConnectionViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConnectionViewHolder holder, int position) {
+        Connection connection = connections.get(position);
+        holder.bind(connection, context);
+        bindButtons(holder, connection);
+    }
+
+    private void bindButtons(ConnectionViewHolder holder, Connection connection) {
+        holder.itemView.setOnClickListener(v -> Toast.makeText(context, "@TODO", Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public int getItemCount() {
+        return connections.size();
+    }
+
+    public static class ConnectionViewHolder extends RecyclerView.ViewHolder {
+        private final TextView connectionName;
+        private final ImageView connectionIcon;
+
+        public ConnectionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            connectionName = itemView.findViewById(R.id.connectionName);
+            connectionIcon = itemView.findViewById(R.id.connectionIcon);
+        }
+
+        public void bind(Connection connection, Context context) {
+            connectionName.setText(connection.getName());
+            connectionIcon.setImageResource(connection.getDrawable());
+        }
+    }
+}
