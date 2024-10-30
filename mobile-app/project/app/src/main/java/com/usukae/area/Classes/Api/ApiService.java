42c17fea0d5e5033936469dec427baf4b23d0e0d
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-10-30 15:45:38 +0100

    update: ApiService with new protocol

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Api/ApiService.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Api/ApiService.java
@@ -1,16 +1,24 @@
 package com.usukae.area.Classes.Api;
 
+import com.usukae.area.Classes.ActionReaction.ActionReaction;
+import com.usukae.area.Classes.ActionReaction.ActionReactionRequest;
 import com.usukae.area.Classes.Areas.AreasAuthUrlResponse;
 import com.usukae.area.Classes.Auth.Login.LoginRequest;
 import com.usukae.area.Classes.Auth.Login.LoginResponse;
 import com.usukae.area.Classes.Auth.Register.RegisterRequest;
 import com.usukae.area.Classes.Auth.Register.RegisterResponse;
+import com.usukae.area.Classes.Auth.User.UserResponse;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Url;
 
 public interface ApiService {
@@ -32,4 +40,22 @@ public interface ApiService {
 
     @GET
     Call<Void> getCheckConnection(@Url String url, @Header("Authorization") String token);
+
+    @GET("/users/infos")
+    Call<UserResponse> getUserInfo(@Header("Authorization") String token);
+
+    @PUT("/users/update")
+    Call<Void> updateUser(@Header("Authorization") String token, @Body UserResponse user);
+
+    @DELETE("/spotify/logout")
+    Call<Void> logoutSpotify(@Header("Authorization") String token);
+
+    @POST("/manage/add-action-reaction")
+    Call<Void> addActionReaction(@Header("Authorization") String token, @Body ActionReactionRequest actionReactionRequest);
+
+    @GET("/manage/get-action-reaction")
+    Call<List<ActionReaction>> getAllActionReactions(@Header("Authorization") String token);
+
+    @DELETE("/manage/delete-action-reaction/{id}")
+    Call<Void> deleteActionReaction(@Header("Authorization") String token, @Path("id") String id);
 }
