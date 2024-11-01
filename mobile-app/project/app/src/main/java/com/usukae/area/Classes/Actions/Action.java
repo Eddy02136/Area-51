2f5c78d7b4314f833b1bcf7dd4e24614bce5ebe9
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-11-01 14:47:53 +0100

    update: type of action

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Actions/Action.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Actions/Action.java
@@ -3,28 +3,22 @@ package com.usukae.area.Classes.Actions;
 import java.util.Map;
 
 public class Action {
-    private final String id;
+    private final String service;
     private final String name;
-    private final String description;
     private Map<String, String> parameters;
 
-    public Action(String id, String name, String description, Map<String, String> parameters) {
-        this.id = id;
+    public Action(String service, String name, Map<String, String> parameters) {
+        this.service = service;
         this.name = name;
-        this.description = description;
         this.parameters = parameters;
     }
 
-    public String getId() {
-        return id;
+    public String getService() {
+        return service;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public Map<String, String> getParameters() {
