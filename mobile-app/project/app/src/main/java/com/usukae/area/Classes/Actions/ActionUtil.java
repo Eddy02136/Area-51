ce61d998970459e07b011df0bc64e6f3d480c74e
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-11-01 14:48:41 +0100

    remove: local loading of actions and reactions

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Actions/ActionUtil.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Actions/ActionUtil.java
@@ -2,30 +2,7 @@ package com.usukae.area.Classes.Actions;
 
 import com.usukae.area.R;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class ActionUtil {
-    public List<Action> getActions() {
-        List<Action> actionsList = new ArrayList<>();
-
-        Map<String, String> issParams = new HashMap<>();
-        issParams.put("city", "");
-
-        Action issAction = new Action(
-                "iss_get_pos",
-                "ISS Position",
-                "Obtient la position actuelle de l'ISS.",
-                issParams
-        );
-
-        actionsList.add(issAction);
-
-        return actionsList;
-    }
-
     public int getActionIcon(String id) {
         switch (id.toLowerCase()) {
             case "iss_get_pos":
--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Reactions/ReactionUtil.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Reactions/ReactionUtil.java
@@ -2,31 +2,7 @@ package com.usukae.area.Classes.Reactions;
 
 import com.usukae.area.R;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class ReactionUtil {
-    public List<Reaction> getReactions() {
-        List<Reaction> reactionsList = new ArrayList<>();
-
-        Map<String, String> spotifyParams = new HashMap<>();
-        spotifyParams.put("music", "");
-
-        Reaction spotifyReaction = new Reaction(
-                "spotify_play_music",
-                "Spotify Play Music",
-                "Joue une musique sur Spotify.",
-                spotifyParams
-        );
-
-        reactionsList.add(spotifyReaction);
-
-        return reactionsList;
-    }
-
-
     public int getReactionIcon(String id) {
         switch (id.toLowerCase()) {
             case "spotify_play_music":
