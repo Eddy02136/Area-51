261e2fe2dc73c97dcf3dfd90a5baeb68c5c2edce
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-11-01 14:47:48 +0100

    update: type of reaction

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Reactions/Reaction.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Reactions/Reaction.java
@@ -3,28 +3,22 @@ package com.usukae.area.Classes.Reactions;
 import java.util.Map;
 
 public class Reaction {
-    private final String id;
+    private final String service;
     private final String name;
-    private final String description;
     private Map<String, String> parameters;
 
-    public Reaction(String id, String name, String description, Map<String, String> parameters) {
-        this.id = id;
+    public Reaction(String service, String name, Map<String, String> parameters) {
+        this.service = service;
         this.name = name;
-        this.description = description;
         this.parameters = parameters;
     }
 
-    public String getId() {
-        return id;
+    public String getService() {
+        return service;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public Map<String, String> getParameters() {
