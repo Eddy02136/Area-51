2f4d3df8657184ba11deead402fe0365919e1739
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-11-03 07:19:50 +0100

    add: camelCase format method

--- mobile-app/project/app/src/main/java/com/usukae/area/Classes/Utils/TextUtil.java
+++ mobile-app/project/app/src/main/java/com/usukae/area/Classes/Utils/TextUtil.java
@@ -16,4 +16,14 @@ public class TextUtil {
     public boolean isValidPassword(String password) {
         return password.length() >= 5 && password.length() <= 25;
     }
+
+    static public String formatCamelCase(String input) {
+        String result = input.replaceAll("(?<!^)([A-Z])", " $1");
+        String[] words = result.split(" ");
+        StringBuilder formattedString = new StringBuilder();
+        for (String word : words) {
+            formattedString.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
+        }
+        return formattedString.toString().trim();
+    }
 }
